4f3ce728e794cbd9b8350abd83175882e04a037b
STpiamcee42 <mechanic_liang@126.com>
2018-01-22 15:03:26 +0800

    功能实现

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <stdlib.h>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
@@ -6,93 +9,134 @@ using namespace cv;
 
 #define RATIO_OF_CHESS 0.0765625f
 
+void screenShot(void);
+void pressScreen(int ms);
+void delay(int ms);
+
 int main(int argc, char* argv[])
 {
-	int xChess = 0;
-	int xBlock = 0;
-	
-	Mat srcImage = imread("test1.png");
-	resize(srcImage, srcImage, Size(srcImage.cols / 2, srcImage.rows / 2));
-	imshow("src", srcImage);
+	cout << "Be sure that you have open the Jumpin-Jump, press any key to continue." << endl;
+	getchar();
 
-	//find chess by template matching
-	Mat templImage = imread("template.jpg");
-	resize(templImage, templImage, Size((int)(srcImage.cols * RATIO_OF_CHESS), (int)(srcImage.cols * RATIO_OF_CHESS * 2.57f)));
-	Mat resultImage = Mat::zeros(srcImage.rows - templImage.rows + 1, srcImage.rows - templImage.rows + 1, CV_32FC1);
-
-	matchTemplate(srcImage, templImage, resultImage, CV_TM_SQDIFF);
-	normalize(resultImage, resultImage, 0, 1, NORM_MINMAX, -1, Mat());
-
-	double minValue;
-	double maxValue;
-	Point minLocation;
-	Point maxLocation;
-	Mat chessPosiImage = srcImage.clone();
-	minMaxLoc(resultImage, &minValue, &maxValue, &minLocation, &maxLocation, Mat());
-	rectangle(chessPosiImage, minLocation, Point(minLocation.x + templImage.cols - 1, minLocation.y + templImage.rows - 1), Scalar(0, 255, 0));
-	xChess = ((minLocation.x + templImage.cols - 1) + minLocation.x) / 2;
-	imshow("chess", chessPosiImage);
-
-	//find block
-	Mat hsvImage;
-	cvtColor(srcImage, hsvImage, COLOR_BGR2HSV_FULL);
-	imshow("hsv", hsvImage);
-
-	Vec3b pix = hsvImage.ptr<Vec3b>(hsvImage.rows / 15)[hsvImage.cols];
-	hsvImage.forEach<Vec3b>([pix, hsvImage, xChess](Vec3b& pixel, const int* position) -> void
+	while (true)
 	{
-		if (fabs(pixel[0] - pix[0]) < 10 && fabs(pixel[1] - pix[1]) < 40)
-		{
-			pixel[0] = 0;
-			pixel[1] = 0;
-			pixel[2] = 0;
-		}
-		if (xChess < hsvImage.cols / 2)
-		{
-			if (position[0] < hsvImage.rows / 5 || position[0] > hsvImage.rows / 2 || position[1] < hsvImage.cols / 2)
-			{
-				pixel[0] = 0;
-				pixel[1] = 0;
-				pixel[2] = 0;
-			}
-		}
-		else
-		{
-			if (position[0] < hsvImage.rows / 5 || position[0] > hsvImage.rows / 2 || position[1] > hsvImage.cols / 2)
-			{
-				pixel[0] = 0;
-				pixel[1] = 0;
-				pixel[2] = 0;
-			}
-		}
-	});
-	imshow("after", hsvImage);
+		screenShot();
 
-	int pointCounter = 0;
-	for (int i = 0; i < hsvImage.rows; i++)
-	{
-		for (int j = 0; j < hsvImage.cols; j++)
-		{
-			Vec3b pix = hsvImage.ptr<Vec3b>(i)[j];
-			Vec3b pixNext = hsvImage.ptr<Vec3b>(i)[j + 1];
-			if (pix[0] != 0 || pix[1] != 0 || pix[2] != 0)
-			{
-				xBlock += j;
-				pointCounter++;
+		int xChess = 0;
+		int xBlock = 0;
+		int xDistance = 0;
 
-				if (j + 1 >= hsvImage.cols || (pixNext[0] == 0 && pixNext[1] == 0 && pixNext[2] == 0))
+		Mat srcImage = imread("autojump.png");
+		resize(srcImage, srcImage, Size(srcImage.cols / 2, srcImage.rows / 2));
+		//imshow("src", srcImage);
+
+		//find chess by template matching
+		Mat templImage = imread("template.jpg");
+		resize(templImage, templImage, Size((int)(srcImage.cols * RATIO_OF_CHESS), (int)(srcImage.cols * RATIO_OF_CHESS * 2.57f)));
+		Mat resultImage = Mat::zeros(srcImage.rows - templImage.rows + 1, srcImage.rows - templImage.rows + 1, CV_32FC1);
+
+		matchTemplate(srcImage, templImage, resultImage, CV_TM_SQDIFF);
+		normalize(resultImage, resultImage, 0, 1, NORM_MINMAX, -1, Mat());
+
+		double minValue;
+		double maxValue;
+		Point minLocation;
+		Point maxLocation;
+		Mat chessPosiImage = srcImage.clone();
+		minMaxLoc(resultImage, &minValue, &maxValue, &minLocation, &maxLocation, Mat());
+		rectangle(chessPosiImage, minLocation, Point(minLocation.x + templImage.cols - 1, minLocation.y + templImage.rows - 1), Scalar(0, 255, 0));
+		xChess = ((minLocation.x + templImage.cols - 1) + minLocation.x) / 2;
+		//imshow("chess", chessPosiImage);
+
+		//find block
+		Mat cannyImage;
+		Mat grayImage;
+		cvtColor(srcImage, grayImage, COLOR_BGR2GRAY);
+		Canny(grayImage, cannyImage, 5, 15);
+		//imshow("canny", cannyImage);
+
+		uchar pix = cannyImage.ptr<uchar>(cannyImage.rows / 15)[cannyImage.cols];
+		cannyImage.forEach<uchar>([pix, cannyImage, xChess](uchar& pixel, const int* position) -> void
+		{
+			if (xChess < cannyImage.cols / 2)
+			{
+				if (position[0] < cannyImage.rows / 5 || position[0] > cannyImage.rows / 2 || position[1] < cannyImage.cols / 2)
 				{
-					xBlock /= pointCounter;
-					break;
+					pixel = 0;
 				}
 			}
-		}
-		if (xBlock != 0) { break; }
-	}
+			else
+			{
+				if (position[0] < cannyImage.rows / 5 || position[0] > cannyImage.rows / 2 || position[1] > cannyImage.cols / 2)
+				{
+					pixel = 0;
+				}
+			}
+		});
+		imshow("after", cannyImage);
+		waitKey(1);
 
-	line(srcImage, Point(xChess, srcImage.rows / 2), Point(xBlock, srcImage.rows / 2), Scalar(0, 0, 255), 2);
-	imshow("distance", srcImage);
+		int pointCounter = 0;
+		for (int i = 0; i < cannyImage.rows; i++)
+		{
+			for (int j = 0; j < cannyImage.cols; j++)
+			{
+				uchar pix = cannyImage.ptr<uchar>(i)[j];
+				uchar pixNext = cannyImage.ptr<uchar>(i)[j + 1];
+				if (pix != 0)
+				{
+					xBlock += j;
+					pointCounter++;
+
+					if (j + 1 >= cannyImage.cols || pixNext == 0)
+					{
+						xBlock /= pointCounter;
+						break;
+					}
+				}
+			}
+			if (xBlock != 0) { break; }
+		}
+
+		line(srcImage, Point(xChess, srcImage.rows / 2), Point(xBlock, srcImage.rows / 2), Scalar(0, 0, 255), 2);
+		imshow("distance", srcImage);
+		waitKey(1);
+
+		xDistance = abs(xBlock - xChess);
+		cout << "distance = " << xDistance << endl;
+		int time = static_cast<int>(3.2f * (float)xDistance);
+		cout << "time = " << time << endl;
+		pressScreen(time);
+		int randomDelay = rand() % 200;
+		cout << "random delay = " << randomDelay << endl;
+		delay(1000 + randomDelay);
+	}
 
 	waitKey(0);
 	return 0;
+}
+
+string cmd;
+void screenShot(void)
+{
+	cmd = "adb shell screencap -p /sdcard/autojump.png";
+	system(cmd.data());
+	cmd = "adb pull /sdcard/autojump.png .";
+	system(cmd.data());
+}
+
+void pressScreen(int ms)
+{
+	cmd = "adb shell input swipe 300 300 300 300 ";
+	char time[10];
+	sprintf(time, "%d", ms);
+	string timeStr(time);
+	cmd += timeStr;
+	system(cmd.data());
+}
+
+void delay(int ms)
+{
+	clock_t now = clock();
+	while (clock() - now < ms);
 }
